349c164b4fefbd46976a489fc1ed6ca87bf564e7
astero77 <januszdomek99@wp.pl>
2019-05-21 17:12:09 +0200

    revert

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@
 int main()
 {
 	std::cout << "Hello World" << std::endl;
+<<<<<<< HEAD
 	std::cout << "Hello Earth" << std::endl;
 	std::cout << "Hello Moon" << std::endl;
+=======
+>>>>>>> parent of 29bbf61... Second world
 	return 1;
 }
